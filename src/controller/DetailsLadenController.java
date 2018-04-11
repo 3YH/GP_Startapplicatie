bd3d1635aac6c7ae776afb3af838a83bb1a82672
tloader11 <tristan-floyd@hotmail.com>
2018-04-11 22:19:56 +0200

    dialog werkt

--- src/controller/DetailsLadenController.java
+++ src/controller/DetailsLadenController.java
@@ -3,6 +3,7 @@ package controller;
 import model.PrIS;
 import model.RoosterData;
 import model.klas.Klas;
+import model.persoon.Docent;
 import model.persoon.Student;
 import server.Conversation;
 import server.Handler;
@@ -69,6 +70,46 @@ public class DetailsLadenController implements Handler {
                     }
 
                 }
+            }
+            String lJsonOut = lJsonObjectBuilder.build().toString();
+            conversation.sendJSONMessage(lJsonOut);										// terugsturen naar de Polymer-GUI!	}
+        }
+        else if(lJsonObjIn != null && lJsonObjIn.containsKey("rol") && lJsonObjIn.getString("rol").equals("docent"))
+        {
+            String email = lJsonObjIn.getString("leerlingnummer");                        // Uitlezen van meegestuurde leerlingnummer
+            String lWachtwoord = lJsonObjIn.getString("password");                               //uitlezen van MD5 pass.
+
+            String start = lJsonObjIn.getString("start");
+            String end = lJsonObjIn.getString("end");
+            String title = lJsonObjIn.getString("title");
+
+            Docent d = this.infoSys.getDocent(email);
+
+            if(d!=null && d.komtWachtwoordOvereen(lWachtwoord))
+            {
+                ArrayList<RoosterData> rooster = this.infoSys.getRoosterDataDocent(email);
+                for(RoosterData data : rooster)
+                {
+                    String data_title = data.getSubject();
+                    String start_datetime = data.getDate()+"T"+ data.getStarttime()+":00";
+                    String end_datetime = data.getDate()+"T"+ data.getEndtime()+":00";
+
+                    if(data_title.equals(title) && start_datetime.equals(start) && end_datetime.equals(end))
+                    {
+                        lJsonObjectBuilder.add("cursuscode", data.getGroup_code()); //good login
+                        lJsonObjectBuilder.add("lokaal", data.getLocation());
+                        lJsonObjectBuilder.add("datum", data.getDateNL());
+                        lJsonObjectBuilder.add("starttijd", data.getStarttime());
+                        lJsonObjectBuilder.add("eindtijd", data.getEndtime());
+                        lJsonObjectBuilder.add("docentemail", data.getTeacher_email());
+                        //lJsonObjectBuilder.add("klas", d.getKlasnaam());
+                    }
+
+                }
+            }
+            else
+            {
+                lJsonObjectBuilder.add("rol", "undefined"); //HA. FAKE LOGIN busted...
             }
             String lJsonOut = lJsonObjectBuilder.build().toString();
             conversation.sendJSONMessage(lJsonOut);										// terugsturen naar de Polymer-GUI!	}
